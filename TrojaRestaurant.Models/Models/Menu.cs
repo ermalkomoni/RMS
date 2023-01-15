d203e9c21c6e301d200008dbe796afae1e0d1b4c
Ermal <Ermal@DESKTOP-97HSQT1>
2023-01-15 23:21:24 +0100

    Adding Menu Controller with View

---
+++ TrojaRestaurant.Models/Models/Menu.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TrojaRestaurant.Models
+{
+    public class Menu
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public double Price { get; set; }
+        [Required]
+        public string ImageUrl { get; set; }
+        [Required]
+        public int CategoryId { get; set; }     //foreign key
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }  //foreign key
+    }
+}
---
+++ TrojaRestaurant.Models/ViewModels/MenuViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrojaRestaurant.Models.ViewModels
+{
+    public class MenuViewModel
+    {
+        public Menu Menu { get; set; }
+        public IEnumerable<SelectListItem> CategoryList { get; set; }
+    }
+}
